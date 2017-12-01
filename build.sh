0632f343cfa7082116296e968845bf23978531b4
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 23:06:19 +0530

    docker

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-
 #!/bin/bash
-# sudo apt-get install mono-complete
-# wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-# chmod 755 nuget.exe
+set -ev
 nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json"
 dotnet restore ExpressBase.Core.sln --configfile /home/travis/.config/NuGet/NuGet.Config
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
+dotnet build -c Release ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+TAG=$1	
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+# Create publish artifact
+dotnet publish -c Release --output ../obj/Docker/publish/ebweb ./ExpressBase.Web/ExpressBase.Web.csproj
+dotnet publish -c Release --output ../obj/Docker/publish/ebweb ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
+
+# Build the Docker images
+docker build -t us.gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
+docker build -t us.gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
+
+docker tag us.gcr.io/avian-silo-186815/ebweb:$TAG us.gcr.io/avian-silo-186815/ebweb:latest
+docker tag us.gcr.io/avian-silo-186815/ebss:$TAG us.gcr.io/avian-silo-186815/ebss:latest
+
+# Login to Docker Hub and upload images
+# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+# docker push ahammedunni/expressbaseweb:$TAG
+# docker push ahammedunni/expressbaseweb:latest
