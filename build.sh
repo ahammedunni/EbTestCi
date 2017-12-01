e0cf27f6b3e20277a920f00996ae162cdbaa23ec
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 21:36:12 +0530

    cat no quote

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # sudo apt-get install mono-complete
 # wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 # chmod 755 nuget.exe
-nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/" -Verbosity detailed
-cat /home/travis/.config/NuGet/NuGet.Config
-nuget restore ExpressBase.Core.sln -configfile "/home/travis/.config/NuGet/NuGet.Config"
+nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/" -Verbosity detailed
+nuget restore ExpressBase.Core.sln -configfile /home/travis/.config/NuGet/NuGet.Config
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
