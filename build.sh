f7d06b3ffc5b96b2041be354349520e2e9706c63
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 21:22:29 +0530

    nuget config

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 # wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 # chmod 755 nuget.exe
 nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
-nuget restore ExpressBase.Core.sln
+nuget restore ExpressBase.Core.sln -configfile "/home/travis/.config/NuGet/NuGet.Config"
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
