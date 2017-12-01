1e6e3c595b676038eb95e0dcc9e52a2002645058
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 20:29:24 +0530

    Ci

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 nuget sources add -NonInteractive -Name ebtest-nuget -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
-dotnet restore ./ExpressBase.Web/ExpressBase.Web.csproj
+nuget restore ExpressBase.Core.sln
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
