1e5f0c142fcb1cb7fa97ac19621ff21cbfa65931
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 20:52:45 +0530

    ./nuget

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 chmod 755 nuget.exe
-nuget.exe sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
-nuget.exe restore ExpressBase.Core.sln
+./nuget.exe sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
+./nuget.exe restore ExpressBase.Core.sln
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
