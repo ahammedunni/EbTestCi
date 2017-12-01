16c15e49b2f12e5e866bc3dc1e20a748b98342f0
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 20:59:54 +0530

    mono install

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
+sudo apt-get install mono-complete
 wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 chmod 755 nuget.exe
 ./nuget.exe sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
