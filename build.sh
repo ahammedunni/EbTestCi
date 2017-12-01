13e9f208be86527cfb2af3742643ec6857f75feb
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 21:12:02 +0530

    nuget version 4.4.1

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
-sudo apt-get install mono-complete
-wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-chmod 755 nuget.exe
-./nuget.exe sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
-./nuget.exe restore ExpressBase.Core.sln
+# sudo apt-get install mono-complete
+# wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+# chmod 755 nuget.exe
+nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
+nuget restore ExpressBase.Core.sln
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
