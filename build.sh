18d02423c6571c4a9fa91c1331aebdd3dddf8170
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-01 20:41:31 +0530

    nuget install

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
+sudo apt-get install nuget
 nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json" -Verbosity detailed
 nuget restore ExpressBase.Core.sln
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
