573ae3a3a29a1edde39807b2e234984376bc05e8
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-05 00:07:23 +0530

    Key_file

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,10 @@ docker tag $DOCKER_SERVER/ebweb:$TAG $DOCKER_SERVER/ebweb:latest
 docker tag $DOCKER_SERVER/ebss:$TAG $DOCKER_SERVER/ebss:latest
 # Login to Docker Hub and upload images
 
-echo $GCLOUD_KEY | base64 --decode > ./keyfile.json
-cat ./keyfile.json
-gcloud auth activate-service-account $GCLOUD_EMAIL --key-file ./keyfile.json --project compelling-weft-188014 
+echo $GCLOUD_KEY | base64 --decode > ./gcloud.p12
+cat $GCLOUD_KEY
+cat ./gcloud.p12
+gcloud auth activate-service-account $GCLOUD_EMAIL --key-file $KEY_FILE --project compelling-weft-188014 
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 
 gcloud container clusters get-credentials $GOOGLE_APPLICATION_CREDENTIALS
