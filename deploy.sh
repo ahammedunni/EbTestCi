d9e9f9e98aed9f2f8f2577171e040d78c41a17f6
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-04 15:47:49 +0530

    GCP auth

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ dotnet publish -c Release --output obj/Docker/publish/ebweb ./ExpressBase.Web/Ex
 dotnet publish -c Release --output obj/Docker/publish/ebss ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
 
 # Build the Docker images
-docker build -t asia.gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
-docker build -t asia.gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
+docker build -t gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
+docker build -t gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
 
-docker tag asia.gcr.io/avian-silo-186815/ebweb:$TAG asia.gcr.io/avian-silo-186815/ebweb:latest
-docker tag asia.gcr.io/avian-silo-186815/ebss:$TAG asia.gcr.io/avian-silo-186815/ebss:latest
+docker tag gcr.io/avian-silo-186815/ebweb:$TAG gcr.io/avian-silo-186815/ebweb:latest
+docker tag gcr.io/avian-silo-186815/ebss:$TAG gcr.io/avian-silo-186815/ebss:latest
 
 # Login to Docker Hub and upload images
 # docker login -u oauth2accesstoken -p "$DOCKER_PASSWORD"
 
-# docker push asia.gcr.io/avian-silo-186815/ebweb:$TAG
-# docker push asia.gcr.io/avian-silo-186815/ebss:$TAG
-# docker push asia.gcr.io/avian-silo-186815/ebweb:latest
-# docker push asia.gcr.io/avian-silo-186815/ebss:latest
+docker push gcr.io/avian-silo-186815/ebweb:$TAG
+docker push gcr.io/avian-silo-186815/ebss:$TAG
+docker push gcr.io/avian-silo-186815/ebweb:latest
+docker push gcr.io/avian-silo-186815/ebss:latest
