25319f708c665bd3bb6a75e703927c38d3c84455
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-04 21:11:43 +0530

    gcloud push

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 nuget sources add -NonInteractive -Name MyGet -Source "https://www.myget.org/F/ebtest-nuget/api/v3/index.json"
+
 dotnet restore ExpressBase.Core.sln --configfile /home/travis/.config/NuGet/NuGet.Config
+
 dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
 dotnet build -c Release ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
--- deploy.sh
+++ deploy.sh
@@ -8,27 +8,23 @@ dotnet publish -c Release --output obj/Docker/publish/ebweb ./ExpressBase.Web/Ex
 dotnet publish -c Release --output obj/Docker/publish/ebss ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
 
 # # Build the Docker images
-# docker build -t asia.gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
-# docker build -t asia.gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
+docker build -t $GCP_CONTAINER/ebweb:$TAG ./ExpressBase.Web/.
+docker build -t $GCP_CONTAINER/ebss:$TAG ./ExpressBase.ServiceStack/.
 
-# docker tag asia.gcr.io/avian-silo-186815/ebweb:$TAG asia.gcr.io/avian-silo-186815/ebweb:latest
-# docker tag asia.gcr.io/avian-silo-186815/ebss:$TAG asia.gcr.io/avian-silo-186815/ebss:latest
-
-# # Login to Docker Hub and upload images
-
-# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:$TAG  > /dev/null
-# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:$TAG  > /dev/null
-# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:latest  > /dev/null
-# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:latest  > /dev/null
-
-
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_SERVER
-
-docker build -t $DOCKER_SERVER/ebweb:$TAG ./ExpressBase.Web/.
-docker build -t $DOCKER_SERVER/ebss:$TAG ./ExpressBase.ServiceStack/.
-
+docker tag $GCP_CONTAINER/ebweb:$TAG $GCP_CONTAINER/ebweb:latest
+docker tag $GCP_CONTAINER/ebss:$TAG $GCP_CONTAINER/ebss:latest
+docker tag $GCP_CONTAINER/ebweb:$TAG $DOCKER_SERVER/ebweb:$TAG
+docker tag $GCP_CONTAINER/ebss:$TAG $DOCKER_SERVER/ebss:$TAG
 docker tag $DOCKER_SERVER/ebweb:$TAG $DOCKER_SERVER/ebweb:latest
 docker tag $DOCKER_SERVER/ebss:$TAG $DOCKER_SERVER/ebss:latest
+# Login to Docker Hub and upload images
+
+gcloud docker -- push $GCP_CONTAINER/ebweb:$TAG  > /dev/null
+gcloud docker -- push $GCP_CONTAINER/ebss:$TAG  > /dev/null
+gcloud docker -- push $GCP_CONTAINER/ebweb:latest  > /dev/null
+gcloud docker -- push $GCP_CONTAINER/ebss:latest  > /dev/null
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_SERVER
 
 docker push $DOCKER_SERVER/ebss:latest
 docker push $DOCKER_SERVER/ebss:$TAG
