bf86834399c5f7f9a6f0ef030beb48093951cd33
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-04 18:13:56 +0530

    > /dev

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ docker tag gcr.io/avian-silo-186815/ebss:$TAG gcr.io/avian-silo-186815/ebss:late
 # Login to Docker Hub and upload images
 # docker login -u oauth2accesstoken -p "$DOCKER_PASSWORD"
 
-gcloud docker --push gcr.io/avian-silo-186815/ebweb:$TAG
-gcloud docker --push gcr.io/avian-silo-186815/ebss:$TAG
-gcloud docker --push gcr.io/avian-silo-186815/ebweb:latest
-gcloud docker --push gcr.io/avian-silo-186815/ebss:latest
+gcloud docker push gcr.io/avian-silo-186815/ebweb:$TAG  > /dev/null
+gcloud docker push gcr.io/avian-silo-186815/ebss:$TAG  > /dev/null
+gcloud docker push gcr.io/avian-silo-186815/ebweb:latest  > /dev/null
+gcloud docker push gcr.io/avian-silo-186815/ebss:latest  > /dev/null
