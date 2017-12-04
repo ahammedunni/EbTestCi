8ac7b6296b04968f8b5dc06372d543734d285094
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-04 19:08:54 +0530

    google to azure

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,30 @@ TAG=$1
 dotnet publish -c Release --output obj/Docker/publish/ebweb ./ExpressBase.Web/ExpressBase.Web.csproj
 dotnet publish -c Release --output obj/Docker/publish/ebss ./ExpressBase.ServiceStack/ExpressBase.ServiceStack.csproj
 
-# Build the Docker images
-docker build -t asia.gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
-docker build -t asia.gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
+# # Build the Docker images
+# docker build -t asia.gcr.io/avian-silo-186815/ebweb:$TAG ./ExpressBase.Web/.
+# docker build -t asia.gcr.io/avian-silo-186815/ebss:$TAG ./ExpressBase.ServiceStack/.
 
-docker tag asia.gcr.io/avian-silo-186815/ebweb:$TAG asia.gcr.io/avian-silo-186815/ebweb:latest
-docker tag asia.gcr.io/avian-silo-186815/ebss:$TAG asia.gcr.io/avian-silo-186815/ebss:latest
+# docker tag asia.gcr.io/avian-silo-186815/ebweb:$TAG asia.gcr.io/avian-silo-186815/ebweb:latest
+# docker tag asia.gcr.io/avian-silo-186815/ebss:$TAG asia.gcr.io/avian-silo-186815/ebss:latest
 
-# Login to Docker Hub and upload images
+# # Login to Docker Hub and upload images
 
-gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:$TAG  > /dev/null
-gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:$TAG  > /dev/null
-gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:latest  > /dev/null
-gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:latest  > /dev/null
+# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:$TAG  > /dev/null
+# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:$TAG  > /dev/null
+# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebweb:latest  > /dev/null
+# gcloud docker -- push asia.gcr.io/avian-silo-186815/ebss:latest  > /dev/null
+
+
+docker login -u ebraviscitest -p s2Bue=CSuqsjYkmBDaewkEc9k1I3b6wD
+
+docker build -t ebraviscitest.azurecr.io/ebweb:$TAG ./ExpressBase.Web/.
+docker build -t ebraviscitest.azurecr.io/ebss:$TAG ./ExpressBase.ServiceStack/.
+
+docker tag ebraviscitest.azurecr.io/ebweb:$TAG ebraviscitest.azurecr.io/ebweb:latest
+docker tag ebraviscitest.azurecr.io/ebss:$TAG ebraviscitest.azurecr.io/ebss:latest
+
+docker push ebraviscitest.azurecr.io/ebss:latest
+docker push ebraviscitest.azurecr.io/ebss:$TAG
+docker push ebraviscitest.azurecr.io/ebweb:$TAG
+docker push ebraviscitest.azurecr.io/ebweb:latest
