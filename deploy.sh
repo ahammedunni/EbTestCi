7491fcfae75f04b6d9cef395a8251b386bef602f
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-12-05 11:53:23 +0530

    base 64 decode

--- deploy.sh
+++ deploy.sh
@@ -27,8 +27,7 @@ docker push $DOCKER_SERVER/ebweb:$TAG
 docker push $DOCKER_SERVER/ebweb:latest
 
 # Login to GCP Container Registry and upload images
-echo $GCLOUD_KEY | base64 > keyfile.json
-printenv
+echo $GCLOUD_KEY | base64 --decode > keyfile.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file keyfile.json --project compelling-weft-188014
 #Next Step
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
